3446494224c02a49b23fa8e5758553b41f10ff4d
Marcin Sobieszczanski <marciso@Gargamel.localdomain>
2019-02-16 10:35:32 +0000

    more debug from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ rsync_opts='-var'
 die() { echo "ERROR: $*" 2>&1 ; exit 1 ; }
 
 src_dir=$(cd $(dirname "$(readlink -f "$0")") && git rev-parse --show-toplevel)
-dst_dir=${1?:missing 1st argument: destination directory}
+dst_dir=${1?:missing 1st argument: destination directory (full path)}  # project name = basename(dst_dir)
 
 test -n "${dst_dir}" || die "first argument empty, expected destination directory"
 test -e "${dst_dir}" && die "destination directory already exists; choose a different destination or remove the directory"
@@ -22,4 +22,4 @@ rsync ${rsync_opts} "${src_dir}/rename.sh" "${dst_dir}/rename.sh"
 
 rm -v "${dst_dir}/rename.sh"
 
-( cd "${dst_dir}" && git init && git add . && git commit -m "${new_name} started" )
+( cd "${dst_dir}" && git init && git add -v . && git commit -v -m "${new_name} started" )
